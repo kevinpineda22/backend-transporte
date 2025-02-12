fc52f4716f0a65f927cb9162bd379e71c2553d53
Juan Manuel <juanmerkahorro@gmail.com>
2025-02-12 16:23:18 -0500

    backend melo cre

--- controllers/registroController.js
+++ controllers/registroController.js
@@ -2,11 +2,11 @@
 import supabase from '../config/supabaseClient.js';
 
 export const guardarRegistro = async (req, res) => {
-  const { fecha, tipo_servicio, conductor, placa, fecha_viaje, origen, sedes, total_valor, observacion } = req.body;
+  const { fecha, tipo_formulario, conductor, placa_vehiculo, fecha_viaje, origen, sedes, valor_total, observacion } = req.body;
 
   try {
     const { data, error } = await supabase.from('transporte').insert([
-      { fecha, tipo_servicio, conductor, placa, fecha_viaje, origen, sedes, total_valor, observacion }
+      { fecha, tipo_formulario, conductor, placa_vehiculo, fecha_viaje, origen, sedes, valor_total, observacion }
     ]);
 
     if (error) return res.status(500).json({ error: error.message });
